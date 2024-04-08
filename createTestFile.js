677b19fe1bd41199a8cb1db23806f1aace2f93f9
Tim <ju4nshin@gmail.com>
2024-04-08 17:50:28 +0900

    feat : 테스트 파일 생성 폴더별 생성처리 가능하게 변경

--- createTestFile.js
+++ createTestFile.js
@@ -16,22 +16,68 @@ const prompt = (question) => {
   });
 };
 
-const createTestFiles = async () => {
-  const testName = await prompt("테스트 이름을 입력하세요: ");
-  const testFolder = path.join(process.cwd(), testName);
+const selectFolder = async (folders) => {
+  for (let i = 0; i < folders.length; i++) {
+    console.log(`${i + 1}. ${folders[i]}`);
+  }
 
-  if (!fs.existsSync(testFolder)) {
-    fs.mkdirSync(testFolder, { recursive: true });
+  const userInput = await prompt("폴더를 입력하세요: ");
+  const selectedIndex = parseInt(userInput);
+
+  if (
+    isNaN(selectedIndex) ||
+    selectedIndex < 1 ||
+    selectedIndex > folders.length
+  ) {
+    console.error("잘못된 입력입니다.");
+    return null;
+  }
+
+  return folders[selectedIndex - 1];
+};
+
+const getFolderList = () => {
+  const folders = fs.readdirSync(process.cwd(), { withFileTypes: true });
+  const excludeFolders = ["node_modules", ".git"];
+  return folders
+    .filter(
+      (folder) => folder.isDirectory() && !excludeFolders.includes(folder.name)
+    )
+    .map((folder) => folder.name);
+};
+
+const createTestFiles = async () => {
+  const folderList = getFolderList();
+
+  if (folderList.length === 0) {
+    console.error("폴더가 없습니다.");
+    return;
+  }
+
+  const selectedFolderName = await selectFolder(folderList);
+
+  if (!selectedFolderName) {
+    return;
+  }
+
+  const testName = await prompt("테스트 이름을 입력하세요: ");
+  const testFolderPath = path.join(process.cwd(), selectedFolderName, testName);
+
+  if (!fs.existsSync(testFolderPath)) {
+    fs.mkdirSync(testFolderPath, { recursive: true });
     const functionFileContent = "";
     const testFileContent = `test('${testName} test', () => {\n  // Write your test here\n});`;
     const readmeFileContent = "";
 
-    fs.writeFileSync(path.join(testFolder, "function.js"), functionFileContent);
     fs.writeFileSync(
-      path.join(testFolder, `${testName}.test.js`),
+      path.join(testFolderPath, "function.js"),
+      functionFileContent
+    );
+    fs.writeFileSync(
+      path.join(testFolderPath, `${testName}.test.js`),
       testFileContent
     );
-    fs.writeFileSync(path.join(testFolder, "index.md"), readmeFileContent);
+    fs.writeFileSync(path.join(testFolderPath, "index.md"), readmeFileContent);
 
     console.log(`테스트 폴더 및 파일이 생성되었습니다.`);
   } else {
