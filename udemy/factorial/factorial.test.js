e1c6684614cd63b8715283511b34ce5ffd470553
Tim <ju4nshin@gmail.com>
2024-04-10 20:00:56 +0900

    add : factorial

---
+++ udemy/factorial/factorial.test.js
@@ -0,0 +1,7 @@
+import { factorial } from "./function";
+test("factorial test", () => {
+  expect(factorial(1)).toBe(1);
+  expect(factorial(2)).toBe(2);
+  expect(factorial(4)).toBe(24);
+  expect(factorial(7)).toBe(5040);
+});
